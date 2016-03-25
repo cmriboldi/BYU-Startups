8dae5a7cad48a8e80ef8141e24b789ce4251bd28
Christian Riboldi <criboldi@familysearch.com>
2016-03-25 14:40:00 -0600

    Adds the avatar directive.

--- public/js/modules/avatar/avatar.module.js
+++ public/js/modules/avatar/avatar.module.js
@@ -1,14 +1,55 @@
+/**
+ * 1. We have added a directive with the name 'avatar' and handler of
+ * avatarDirective to our angular app module
+ */
 angular.module('app', [])
-  .controller('mainCtrl', mainCtrl);
+  .controller('mainCtrl', mainCtrl)
+  .directive('avatar', avatarDirective);
 
 function mainCtrl ($scope) {
-  
-  /**
-   * Let's just make sure something happens when the user submits the form
-   * by binding the declared 'addNew' function to the scope. You can see we
-   * are expecting a user object as a parameter. This is 'userForm'
-   */
+
+  $scope.users = [];
+
   $scope.addNew = function (user) {
-    alert(user.name + ' ' + user.url);
+    $scope.users.push({ 
+      name: user.name,
+      avatarUrl: user.url
+    }); /* [1] */
+    
+    user.name = ''; /* [2] */
+    user.url = '';
   };
+}
+
+/**
+ * 1. this defines the api of our avatar directive. This means we are
+ * expecting a user property whose value should be interpreted as an object.
+ * 2. This simply means we want this directive to be used as an element.
+ * 3. You can see here we've moved the html that was in our template before
+ * and give it as the template for this directive. This means wherever we use
+ * <avatar /> this html will also be placed there.
+ * 4. Here we are implementing the feature where if there is no user avatar url,
+ * we go ahead and give it a default
+ */
+function avatarDirective () {
+  return {
+    scope: {
+      user: '=' /* [1] */
+    },
+    restrict: 'E', /* [2] */
+    template: (
+      '<div class="Avatar">' +
+        '<img ng-src="{{user.avatarUrl}}" />' +
+        '<h4>{{user.name}}</h4>' +
+      '</div>'
+    ), /* [3] */
+    link: link
+  };
+  
+  function link (scope) { /* [4] */
+    if (!scope.user.avatarUrl) {
+      scope.user.avatarUrl = 'http://thealmanac.org/assets/img/default_avatar.png';
+    }
+  }
+
 }
