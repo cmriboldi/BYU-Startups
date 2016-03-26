3c901c75c659702c5feb898f5ce9d5cf703482c2
Christian Riboldi <criboldi@familysearch.com>
2016-03-25 19:02:26 -0600

    Adds the passport data.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BYU Startups' });
+var isLoggedin = function (req, res, next) {
+if (req.isAuthenticated())
+    return next();
+res.render('login', {title : "Login Page"});
+}
+
+module.exports = function(passport){
+
+/* GET login page. */
+router.get('/', function(req, res){
+    res.render('index', { title: 'BYU Startups' });
 });
 
-module.exports = router;
+/* Handle Login POST */
+router.post('/login', passport.authenticate('login', {
+    successRedirect: '/profile',
+    failureRedirect: '/'
+}));
+
+/* GET Registration Page */
+router.get('/signup', function(req, res){
+    res.render('signup');
+});
+
+/* GET Profile Page */
+router.get('/profile', isLoggedin, function(req, res){
+    res.render('profile', { title: 'BYU Profile' });
+});
+
+/* Handle Registration POST */
+router.post('/signup', passport.authenticate('signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup'
+}));
+
+/* Handle Logout */
+router.get('/signout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+return router;
+}
