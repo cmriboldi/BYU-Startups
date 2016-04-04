78633d70b452ba691ab186fe18592ca43dd0c46e
Cannon Collins <cannoncollins@Cannons-MacBook-Pro.local>
2016-04-04 00:33:09 -0600

    trying to make nav work

--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
-var isLoggedin = function (req, res, next) {
-if (req.isAuthenticated())
+var isLoggedin = function(req, res, next) {
+  if (req.isAuthenticated())
     return next();
-res.render('login');
+  res.render('login');
 }
 
-module.exports = function(passport){
+module.exports = function(passport) {
 
-/* GET login page. */
-router.get('/', function(req, res){
-    res.render('index', { title: 'BYU Startups', loggedIn: req.isAuthenticated() });
-});
+  /* GET login page. */
+  router.get('/', function(req, res) {
+    res.render('index', {
+      title: 'BYU Startups',
+      loggedIn: req.isAuthenticated()
+    });
+  });
 
-/* Handle Login POST */
-router.post('/login', passport.authenticate('login', {
+  /* Handle Login POST */
+  router.post('/login', passport.authenticate('login', {
     successRedirect: '/profile',
     failureRedirect: '/'
-}));
+  }));
 
-/* GET Login Page */
-router.get('/login', function(req, res){
+  /* GET Login Page */
+  router.get('/login', function(req, res) {
     res.render('login');
-});
+  });
 
-/* GET Registration Page */
-router.get('/signup', function(req, res){
+  /* GET Registration Page */
+  router.get('/signup', function(req, res) {
     res.render('signup');
-});
+  });
 
-/* GET Profile Page */
-router.get('/profile', isLoggedin, function(req, res){
-    res.render('profile', { title: 'BYU Profile' });
-});
+  /* GET Profile Page */
+  router.get('/profile', isLoggedin, function(req, res) {
+    res.render('profile', {
+      title: 'BYU Profile'
+    });
+  });
 
-/* Handle Registration POST */
-router.post('/signup', passport.authenticate('signup', {
+  /* Handle Registration POST */
+  router.post('/signup', passport.authenticate('signup', {
     successRedirect: '/profile',
     failureRedirect: '/signup'
-}));
+  }));
 
-/* Handle Logout */
-router.get('/signout', function(req, res) {
+  /* Handle Logout */
+  router.get('/signout', function(req, res) {
     req.logout();
     res.redirect('/');
-});
+  });
 
-return router;
+  return router;
 }
