ee38543d8f85db644a2e7dabfbfebbc1ee771f6f
Christian Riboldi <criboldi@familysearch.com>
2016-04-07 19:18:51 -0600

    Adds the message user routes.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
+var users = require('../controllers/users.controller')
 
 var isLoggedin = function(req, res, next) {
   if (req.isAuthenticated())
@@ -22,21 +23,33 @@ module.exports = function(passport) {
 
   /* Handle Login POST */
 
-  router.post('/login', passport.authenticate('login', {
-    successRedirect: '/profile',
-    failureRedirect: '/login',
-    failureFlash: true
-  }));
+  // router.post('/login', passport.authenticate('login', {
+  //   successRedirect: '/profile',
+  //   failureRedirect: '/login',
+  //   failureFlash: true
+  // }));
+  
+  app.route('/login')
+     .get(users.renderSignin)
+     .post(passport.authenticate('local', {
+       successRedirect: '/',
+       failureRedirect: '/signin',
+       failureFlash: true
+     }));
 
   /* GET Login Page */
-  router.get('/login', function(req, res) {
-    res.render('login', { message: "" });
-  });
+  // router.get('/login', function(req, res) {
+  //   res.render('login', { message: "" });
+  // });
 
   /* GET Registration Page */
-  router.get('/signup', function(req, res) {
-    res.render('signup');
-  });
+  app.route('/signup')
+     .get(users.renderSignup)
+     .post(users.signup);
+  
+  // router.get('/signup', function(req, res) {
+  //   res.render('signup');
+  // });
 
   /* GET Profile Page */
   router.get('/profile', isLoggedin, function(req, res) {
@@ -52,10 +65,12 @@ module.exports = function(passport) {
   }));
 
   /* Handle Logout */
-  router.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
-  });
+  // router.get('/logout', function(req, res) {
+  //   req.logout();
+  //   res.redirect('/');
+  // });
+  
+  app.get('/logout', users.signout);
 
   router.get('/users', function(req, res, next) {
     console.log(req.body);
@@ -68,74 +83,3 @@ module.exports = function(passport) {
   return router;
 }
 
-
-var getErrorMessage = function(err) {
-  var message = '';
-
-  if (err.code) {
-    switch (err.code) {
-      case 11000:
-      case 11001:
-        message = 'Username already exists';
-        break;
-      default:
-        message = 'Something went wrong';
-    }
-  } else {
-    for (var errName in err.errors) {
-      if (err.errors[errName].message) message = err.errors[errName].message;
-    }
-  }
-
-  return message;
-};
-
-exports.renderSignin = function(req, res, next) {
-  if (!req.user) {
-    res.render('signin', {
-      title: 'Sign-in Form',
-      messages: req.flash('error') || req.flash('info')
-    });
-  } else {
-    return res.redirect('/');
-  }
-};
-exports.renderSignup = function(req, res, next) {
-  if (!req.user) {
-    res.render('signup', {
-      title: 'Sign-up Form',
-      messages: req.flash('error')
-    });
-  } else {
-    return res.redirect('/');
-  }
-};
-
-exports.signup = function(req, res, next) {
-  if (!req.user) {
-    var user = new User(req.body);
-    var message = null;
-
-    user.provider = 'local';
-
-    user.save(function(err) {
-      if (err) {
-        var message = getErrorMessage(err);
-
-        req.flash('error', message);
-        return res.redirect('/signup');
-      }
-      req.login(user, function(err) {
-        if (err) return next(err);
-        return res.redirect('/');
-      });
-    });
-  } else {
-    return res.redirect('/');
-  }
-};
-
-exports.signout = function(req, res) {
-  req.logout();
-  res.redirect('/');
-};
